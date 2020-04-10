4feec3b1ad22b2ad5a1cc2bfbb969a66d2c0b74d
DaniGio <59997215+DaniGio@users.noreply.github.com>
2020-04-10 18:30:48 +1000

    adding line

---
+++ Mapping_Lines/static/js/logic.js
@@ -0,0 +1,103 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with a center and zoom level.
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+
+// Coordinates for each point to be used in the line.
+// let line = [
+// 	[33.9416, -118.4085],
+// 	[37.6213, -122.3790]
+//   ];
+
+// Coordinates for each point to be used in the polyline.
+let line = [
+	[33.9416, -118.4085],
+	[37.6213, -122.3790],
+	[40.7899, -111.9791],
+	[47.4502, -122.3088]
+  ];
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+	color: "yellow"
+  }).addTo(map);
+// // We create the tile layer that will be the background of our map.
+// let streets = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+// attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+// 	maxZoom: 18,
+// 	id: 'mapbox.streets',
+// 	accessToken: API_KEY
+// });
+
+//  Add a marker to the map for Los Angeles, California.
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// L.circle([34.0522, -118.2437], {
+// 	radius: 10000
+//  }).addTo(map);
+
+// //zoom in to a level of 14 on the setView() method.
+// let map = l.map('mapid').setView([34.0522, -118.2437],14);
+
+// L.circle([34.0522, -118.2437], {
+// 	radius: 100
+//  }).addTo(map);
+
+// L.circleMarker([34.0522, -118.2437], {
+// 	radius:300,
+// 	color: "black", 
+// 	fillColor: '#ffffa1'
+// }).addTo(map);
+
+// // Get data from cities.js
+// let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+// cityData.forEach(function(city) {
+// 	console.log(city)
+// 	L.marker(city.location).addTo(map);
+// });
+
+//add popup
+// Loop through the cities array and create one marker for each city.
+// cityData.forEach(function(city) {
+// 	console.log(city)
+// 	L.circleMarker(city.location,{
+// 		radius: city.population/100000,
+// 		color: 'orange',
+// 		lineweight: 4
+// 		})
+// 	.bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+//   .addTo(map);
+// });
+
+// // We create the tile layer that will be the background of our map- street view:
+// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+ // We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}',{
+
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	accessToken: API_KEY
+});
+
+
+
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
+
+// To change the map’s style, change the map id using the list of Mapbox ids below:
+
+// mapbox.streets
+// mapbox.light
+// mapbox.dark
+// mapbox.satellite
+// mapbox.streets-satellite
+// mapbox.wheatpaste
+// mapbox.streets-basic
+// mapbox.comic
+// mapbox.outdoors
+// mapbox.run-bike-hike
+// mapbox.pirates
+// mapbox.emerald
+// mapbox.high-contrast
